4ffd6870f3659fc25c9d0a52bd6ca6ef05873c27
Rodrigo Rodrigues <32945462+rfrl@users.noreply.github.com>
2019-03-08 22:47:15 -0300

    Create fastPrime.cpp

---
+++ Algorithm/Math/fastPrime.cpp
@@ -0,0 +1,15 @@
+vector<int> fastPrime(int lim) {
+    vector<bool> sieve( lim , 1);
+    vector<int> resp;
+    resp.push_back(2);
+    for(int i=3; i<lim; i+=2){
+        if(sieve[i]){
+            resp.push_back(i);
+            if(i*1ll*i>lim) continue;
+            for(int j=i*i; j<lim; j+=i){
+               sieve[j]=0;
+            }
+         }
+      }
+      return resp;
+}
