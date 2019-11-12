d47231396c04ce7255ab0b10f978607b2ec638f1
Rodrigo Rodrigues <32945462+rodz@users.noreply.github.com>
2019-11-11 23:28:20 -0300

    Create kruskal.cpp

---
+++ Algorithm/Graph/kruskal.cpp
@@ -0,0 +1,26 @@
+// By GabrielPessoa
+
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+
+iii e[me], mst[me];
+int z, mstLen;
+
+void add(int u, int v, int w) {
+  e[z++] = iii(w, ii(u, v));
+}
+
+int kruskal() {
+  int ans = 0;
+  dsBuild();
+  sort(e, e + z);
+  for(int i = 0; i < n; i++) {
+    int u = e[i].second.first, v = e[i].second.second, w = e[i].first;
+    if(dsFind(u) != dsFind(v)) {
+      dsUnion(u, v);
+      ans += w;
+      mst[mstLen++] = e[i];
+    }
+  }
+  return ans;
+}
